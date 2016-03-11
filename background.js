ca88cf65fcae7d34b79c74e75b2b8cf7bd7cd534
Colin <colinwillson@gmail.com>
2016-03-11 11:15:48 -0800

    Added real time monitoring and a new icon that changes when you turn it on or off.

--- background.js
+++ background.js
@@ -1,9 +1,28 @@
-var oauth = ChromeExOAuth.initBackgroundPage({
-  'request_url': 'https://www.google.com/accounts/OAuthGetRequestToken',
-  'authorize_url': 'https://www.google.com/accounts/OAuthAuthorizeToken',
-  'access_url': 'https://www.google.com/accounts/OAuthGetAccessToken',
-  'consumer_key': 'anonymous',
-  'consumer_secret': 'anonymous',
-  'scope': 'https://docs.google.com/feeds/',
-  'app_name': 'Unique IQ'
-});
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var current = "icon_on.png";
+
+function updateIcon() {
+  chrome.browserAction.setIcon({path: current});
+
+  if (current === "icon_on.png") {
+    current = "icon_off.png";
+  } else {
+    current = "icon_on.png";
+  }
+
+
+}
+
+chrome.browserAction.onClicked.addListener(updateIcon);
+updateIcon();
+
+
+
+chrome.history.onVisited.addListener(updateLink);
+
+function updateLink(historyItem) {
+
+}
